d67a400b0716ce5eba01e22c3c5261cde34b968b
gpatils <gpatils@gmail.com>
2011-11-24 23:04:16 +0530

    'initial'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '279ea3653c361c32f4d1967d111663daf025d898de16db27ae6e49ca4257f067532d112f34ea4300905d947ad95d89ad9e4671a55a20a1ecc2cdc60df31ce6cc'
